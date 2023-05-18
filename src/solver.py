829f34cb4f3b0a1eb4ff2382ee75d9efcc3ab527
Pietro Bonardi <bonardipietro95@gmail.com>
2023-05-19 00:18:32 +0200

    added solver.py

---
+++ src/solver.py
@@ -0,0 +1,10 @@
+from map import Map
+
+class Solver(): 
+    def __init__(self,grid:Map) -> None:
+        self.grid = grid
+        
+        pass
+
+    def path_finder(self,grid:Map):
+        pass

b5838ae7c65b42ca83cfa68e7db9a7c44fc46981
Pietro Bonardi <bonardipietro95@gmail.com>
2023-11-06 21:35:01 +0100

    restructured map_ui

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+from pathfinder.data_structure.map import Map
 from pathfinder.ui.map_ui import MapUI
 
-def main():
 
-    map = MapUI(10,10)
-    map.draw_map()
-    map.window.mainloop()
+def main():
+    map_ui = MapUI(5, 5)
+    map_ui.create_map()
+    map_ui.window.mainloop()
+    
 
 if __name__ == "__main__":
     main()
--- pathfinder/ui/map_ui.py
+++ pathfinder/ui/map_ui.py
@@ -1,72 +1,61 @@
 import tkinter as tk
+
 from pathfinder.data_structure.map import Map
 from pathfinder.shortcuts import *
-from pathfinder.engines.search import RandomWalk
+# from pathfinder.engines.search import RandomWalk
 
 
-class MapUI():
-
-    def __init__(self, rows: int, cols: int) -> None:
+class MapUI: 
+    # TODO: add solver parameter 
+    def __init__(self, rows:int, cols: int) -> None:
         self.rows = rows
         self.cols = cols
-        self.map = Map(self.rows, self.cols)
+        # gap between logic and ui
+        self.window = tk.Tk()
         self.frames = [[None for _ in range(self.cols)]
                        for _ in range(self.rows)]
-        self.window = tk.Tk()
+        self.map = Map(rows, cols)
+        # self.solver = solver
 
-    def draw_map(self):
-        self.window.configure(bg="black")
-        # self.window.rowconfigure(row, weight=1)  
-        # self.window.columnconfigure(col, weight=1)
-    
+    def create_map(self):
         for row in range(self.rows):
-            self.window.rowconfigure(row, weight=1)  
             for col in range(self.cols):
-                self.window.columnconfigure(col, weight=1)
-                frame = tk.Frame(
-                    master=self.window,
-                    relief=tk.RAISED,
-                    borderwidth=1,
-                    width=100,
-                    height=100,
-                    bg="#444444"
-                )
+                frame = tk.Frame(self.window, width=100, height=100, bg="grey")
                 frame.grid(row=row, column=col, padx=5, pady=5, sticky="nsew")
-               
-                frame.bind("<Button-1>", self.cell_clicked)
+                frame.bind("<Button-1>", self.on_button_click)
                 self.frames[row][col] = frame
-
-        ok_buttom = tk.Button(self.window, text="OK", command=self.random_walk)
-        ok_buttom.grid(row=self.rows, columnspan=self.cols, pady=10)
-
-    def cell_clicked(self, event):
-        # Ottieni la coordinata della cella cliccata
-        cell_widget = event.widget
-        i = cell_widget.grid_info()['row']
-        j = cell_widget.grid_info()['column']
         
-        if self.map.start is None:
-            self.map.insert_element((i, j), START)
-            cell_widget.configure(bg="green")
-        elif self.map.end is None:
-            self.map.insert_element((i, j), END)
-            cell_widget.configure(bg="blue")
-        else:
-            self.map.insert_element((i, j), OBSTACLE)
-            cell_widget.configure(bg="black")
+        clear = tk.Button(self.window, text="clear", command=self.clear)
+        clear.grid(row=self.rows, column=self.cols-1, padx=10, pady=20)
 
-    def random_walk(self):
-        random_walk = RandomWalk(self.map)
-        solution = random_walk.solve()
-        colored_before = None
+        run = tk.Button(self.window, text="run", command=self.run)
+        run.grid(row=self.rows, column=0, padx=10, pady=10)
 
-        delta = 0
-        for i, j in solution:
-            delta += 100
-            if colored_before is not None:
-                tmp_i, tmp_j = colored_before
-                self.window.after(
-                    delta, lambda tmp_i=tmp_i, tmp_j=tmp_j: self.frames[tmp_i][tmp_j].config(bg="#444444"))
-            colored_before = (i, j)
-            self.window.after(delta, lambda i=i,
-                              j=j: self.frames[i][j].config(bg="red"))
+   
+    def on_button_click(self, event) -> None: 
+        cell_widget = event.widget
+        row = cell_widget.grid_info()['row']
+        col = cell_widget.grid_info()['column']
+
+        if self.map.start is None: 
+            self.map.insert_element((row,col), label=START)
+            cell_widget.config(bg="blue")
+
+        elif self.map.end is None: 
+            self.map.insert_element((row,col), label=END)
+            cell_widget.config(bg="red")
+
+        else: 
+            self.map.insert_element((row,col), label=OBSTACLE)
+            cell_widget.config(bg="black")
+    
+    def clear(self):
+        self.map.start, self.map.end = None, None
+        for i in range(self.rows):
+            for j in range(self.cols):
+                self.frames[i][j].config(bg="grey")
+
+    def run(self):
+        # TODO: add solver
+        pass
+

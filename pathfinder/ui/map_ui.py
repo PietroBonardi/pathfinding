35ce41156321fbe612150383f027b13574dfcc8d
Pietro Bonardi <bonardipietro95@gmail.com>
2023-11-07 21:05:30 +0100

    implemented draw_solution

--- pathfinder/ui/map_ui.py
+++ pathfinder/ui/map_ui.py
@@ -1,8 +1,9 @@
 import tkinter as tk
-
 from pathfinder.data_structure.map import Map
 from pathfinder.shortcuts import *
-# from pathfinder.engines.search import RandomWalk
+from pathfinder.engines.search import RandomWalk
+from typing import List, Tuple
+import time
 
 
 class MapUI: 
@@ -15,7 +16,7 @@ class MapUI:
         self.frames = [[None for _ in range(self.cols)]
                        for _ in range(self.rows)]
         self.map = Map(rows, cols)
-        # self.solver = solver
+        self.solver = None
 
     def create_map(self):
         for row in range(self.rows):
@@ -49,13 +50,36 @@ class MapUI:
             self.map.insert_element((row,col), label=OBSTACLE)
             cell_widget.config(bg="black")
     
-    def clear(self):
+    def clear(self) -> None:
         self.map.start, self.map.end = None, None
         for i in range(self.rows):
             for j in range(self.cols):
                 self.frames[i][j].config(bg="grey")
 
-    def run(self):
+    def draw_solution(self, solution: list[Tuple[int,int]]) -> None: 
+        colored_before = None
+        delay = 0
+        start = solution[0]
+        end = solution[-1]
+
+        for i, j in solution:
+            if (i,j) != start and (i,j) != end:
+                delay+=50
+                self.window.after(delay, lambda i = i, j = j : self.frames[i][j].config(bg="white"))
+                if colored_before:
+                    tmp_i, tmp_j = colored_before
+                    self.window.after(delay, lambda tmp_i = tmp_i, tmp_j = tmp_j : self.frames[tmp_i][tmp_j].config(bg="gray"))
+                
+                colored_before = (i,j)
+        
+        self.window.after(delay, lambda tmp_i = colored_before[0], tmp_j = colored_before[1] : self.frames[tmp_i][tmp_j].config(bg="gray"))
+        self.window.after(delay, lambda tmp_i = i, tmp_j = j : self.frames[tmp_i][tmp_j].config(bg="yellow"))
+
+
+
+    def run(self) -> None:
         # TODO: add solver
-        pass
+        self.solver = RandomWalk(self.map)
+        self.draw_solution(self.solver.solve())
+
 
